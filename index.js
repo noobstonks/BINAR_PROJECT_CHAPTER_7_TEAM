9a8f1dbca0f42a872b79cae05bb993461a39558a
noobstonks <androfarlinno@gmail.com>
2021-09-18 10:43:25 +0700

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-//import express
-//import ejs
-//public static express
+
 
 const express = require ('express');
 const ejs = require('ejs')
 const app = express();
 let data = require('./database.json')
+const { user_games, user_biodata, user_histories } = require ('./models');
+app.use(express.urlencoded({extended:false}))
 
 
 const port = 2021;
@@ -13,7 +13,7 @@ const port = 2021;
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+
 
 app.set('view engine', 'ejs');
 
@@ -21,19 +21,6 @@ app.get ('/', (req,res)=>{
     res.status(200).render('home')
 })
 
-
-// app.get('/login', (req,res)=>{
-//     res.render('login')
-// })
-
-// app.post('/login', (req,res)=>{
-//     if(req.body.username.trim() === "andro" && req.body.password.trim() === "test123"){
-//         res.redirect('/')
-//     }
-//     else {
-//         res.send("You're not authorized")
-//     }
-// })
 
 app.get('/login', (req,res)=>{
     res.status(200).render('login')
@@ -43,7 +30,7 @@ app.post('/login', (req,res)=> {
     const username = req.body.username;
     const password = req.body.password;
     if (data.find(o=> o.usernamejson === username && o.passwordjson === password )) {
-        res.redirect('/')
+        res.redirect('/users')
     }
     else {
         res.send("youre not authorized")
@@ -55,21 +42,12 @@ app.get('/game', (req,res)=>{
     res.render('game')
 })
 
+app.get('/users', (req,res)=>{
+    res.render('users/index')
+})
+
 app.listen(port, ()=>{
     console.log("server menyala")
 })
 
-//satu rute dua method, app.get (tampil) - app.post(baca isi)
-
-//yang dilakukan
-//1. bikin form login.ejs , test apakah bisa redirect atau tidak (clear)
-//2. mulai masukkan direktori gambar ke dalam img
-//3. Masukkan direktori skrip (js dan css) ke dalam assets
-//4. Masukkan file index ke dalam home.ejs
-//5. masukkan file paper rock scissors ke dalam home.ejs
-//6. Ubah bootstrap menjadi pemanggilan cdn (gagal)
-
-
-
-//1. membetulkan seluruh source untuk home.ejs
 
