055eb139bf1da74b02b3e87029dff29584b7554c
noobstonks <androfarlinno@gmail.com>
2021-09-18 14:55:53 +0700

    CRUD finished (foreign key not yet)

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ejs = require('ejs')
 const app = express();
 let data = require('./database.json')
 
-const { user_game } = require ('./models');
+const { user_game, user_game_biodata } = require ('./models');
 
 app.use(express.urlencoded({extended:false}))
 
@@ -44,9 +44,6 @@ app.get('/game', (req,res)=>{
     res.render('game')
 })
 
-// app.get('/users', (req,res)=>{
-//     res.render('users/index')
-// })
 
 app.get ('/users', (req,res)=>{
     user_game.findAll()
@@ -56,6 +53,67 @@ app.get ('/users', (req,res)=>{
         })
     })
 })
+
+app.get ('/users/create', (req,res)=>{
+    res.render('users/create')
+})
+
+app.post('/users', (req,res)=>{
+    user_game.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+    user_game_biodata.create({
+        nama: req.body.nama
+    })
+    .then(()=>{
+        res.send('data user berhasil dibuat')
+    })
+})
+
+app.get('/users/:id', (req,res)=>{
+    user_game.findOne({
+        where: {id: req.params.id}
+    })
+    .then(users=>{ 
+        res.render ('users/detail', {
+            users
+        })
+    })
+})
+
+app.get('/users/delete/:id', (req,res)=>{
+    user_game.destroy({ where: {id: req.params.id}})
+    .then(()=>{
+        res.send('data user berhasil dihapus')
+    })
+})
+
+app.get('/users/update/:id', (req,res)=>{
+    user_game.findOne({where: {id: req.params.id}})
+    .then(users=>{ 
+        res.render ('users/update', {
+            users
+        })
+    })
+})
+
+app.post('/users/update/:id', (req,res)=>{
+    user_game.update({
+        username: req.body.username,
+        password: req.body.password
+    },{where: {id:req.params.id}
+})
+    user_game_biodata.update({
+        nama: req.body.nama
+    },{where: {id:req.params.id}
+})
+    .then(()=>{
+        res.send('data user berhasil di update')
+    })
+})
+
+
 
 app.listen(port, ()=>{
     console.log("server menyala")
