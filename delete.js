231e4e86fce3fefa62eb98b5190600810b6d92f4
noobstonks <androfarlinno@gmail.com>
2021-09-18 20:16:31 +0700

    delete -destroy.js-

--- delete.js
+++
@@ -1,9 +0,0 @@
-const {user_game_biodata} = require ('./models')
-
-user_game_biodata.destroy ({
-    where: {
-    id: 12
-}
-})
-
-.then(()=> console.log("Artikel yang belum diapprove telah dihapus"))
--- destroy.js
+++
@@ -1,9 +0,0 @@
-const {user_game} = require ('./models')
-
-user_game.destroy ({
-    where: {
-    id: 11
-}
-})
-
-.then(()=> console.log("Artikel yang belum diapprove telah dihapus"))
